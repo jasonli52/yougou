b8403033a398460ffd64d49e41baa17956a06e32
行者无疆 <252059453@qq.com>
2019-08-16 18:46:56 +0800

    小程序项目首页完成

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //app.js
+import request from "./utils/request.js"
 App({
   onLaunch: function () {
-  
+    //初始化request基准路径
+    request.defaults.baseURL = "https://api.zbztb.cn/api/public/v1";
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,42 @@
-
+import request from "../../utils/request.js"
 Page({
- 
+  data:{
+    autoplay:true,
+    //轮播图的数据
+    imgUrls:[
+    
+    ],
+    menus:[],
+    floors: []
+  },
+  onLoad(){
+    request({
+      url:"/home/swiperdata"
+    }).then(res=>{
+      //返回的数组
+      const {message}=res.data;
+      this.setData({
+        imgUrls:message
+      })
+    }),
+      request({
+      url: "/home/catitems"
+      }).then(res => {
+        //返回的数组
+        const { message } = res.data;
+        this.setData({
+          menus: message
+        })
+      }),
+      // 请求楼层数据
+      request({
+        url: "/home/floordata"
+      }).then(res => {
+        const { message } = res.data;
+
+        this.setData({
+          floors: message
+        })
+      })
+  }
 })
